8179b03f463d5e0f445ea6afe7f38c654fe55059
ns-cn <ns-cn@qq.com>
2022-07-20 14:24:44 +0800

    add consul-bridge

---
+++ Formula/consul-bridge.rb
@@ -0,0 +1,16 @@
+class ConsulBridge < Formula
+  desc "bridge between different consul envirment"
+  homepage "https://github.com/ns-cn/consul-bridge/"
+  url "https://github.com/ns-cn/consul-bridge/releases/download/latest/consul_bridge_darwin"
+  version "1.03"
+  sha256 ""
+
+  def install
+    system "mv consul_bridge_darwin consul-bridge"
+    bin.install "consul-bridge"
+  end
+
+  test do
+    system "false"
+  end
+end
