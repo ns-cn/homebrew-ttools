13edd27a7e62c31dc1d9d556a3d0bb661cd363d1
ns-cn <ns-cn@qq.com>
2022-07-20 13:54:12 +0800

    lines.rb

---
+++ Formula/lines.rb
@@ -0,0 +1,24 @@
+class Lines < Formula
+  desc "charater line tools"
+  homepage "https://github.com/ns-cn/ttools/"
+  url "https://github.com/ns-cn/ttools/releases/download/v.lines%40latest/lines_darwin"
+  version "1.01"
+  sha256 ""
+
+  # depends_on "cmake" => :build
+  depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    system "false"
+  end
+end
