565185d18b6ca96caae86d910da46281376af4b9
ns-cn <ns-cn@qq.com>
2022-07-26 13:17:36 +0800

    lines 1.03

--- Formula/lines.rb
+++ Formula/lines.rb
@@ -1,8 +1,8 @@
 class Lines < Formula
   desc "character line tools"
   homepage "https://github.com/ns-cn/ttools/"
-  url "https://github.com/ns-cn/ttools/releases/download/v.lines-1.02/lines_darwin"
-  version "1.02"
+  url "https://github.com/ns-cn/ttools/releases/download/v.lines-1.03/lines_darwin"
+  version "1.03"
   sha256 ""
 
   def install
