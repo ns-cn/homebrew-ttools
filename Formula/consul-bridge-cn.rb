049c3383c12adbac04f13d2b49120ed227150a4f
ns-cn <ns-cn@qq.com>
2022-07-27 16:22:49 +0800

    update

--- Formula/consul-bridge-cn.rb
+++ Formula/consul-bridge-cn.rb
@@ -1,12 +1,11 @@
 class ConsulBridgeCn < Formula
   desc "bridge between different consul envirment"
   homepage "https://github.com/ns-cn/consul-bridge/"
-  url "https://ns-cn.coding.net/api/user/ns-cn/project/ttools/depot/releases/git/releases/attachments/download/133487"
+  url "https://ns-cn.coding.net/api/user/ns-cn/project/ttools/depot/releases/git/releases/attachments/download/133491"
   version "1.03"
   sha256 ""
 
   def install
-    system "mv consul_bridge_darwin consul-bridge"
     bin.install "consul-bridge"
   end
 
