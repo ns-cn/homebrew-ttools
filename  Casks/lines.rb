6887e4c1873d1a46f77dfc734ffb57fe2ae9fd1e
ns-cn <ns-cn@qq.com>
2022-07-27 15:11:12 +0800

    casks resources

---  Casks/lines.rb
+++  Casks/lines.rb
@@ -1,7 +1,7 @@
 class Lines < Formula
   desc "character line tools"
   homepage "https://github.com/ns-cn/ttools/"
-  url "https://github.com/ns-cn/ttools/releases/download/v.lines-1.03/lines_darwin"
+  url "https://ns-cn.coding.net/p/ttools/d/releases/git/raw/master/lines_darwin?download=true"
   version "1.04"
   sha256 ""
 
