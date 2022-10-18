26c712da27a554fe67afc1dde64f3ab437595ca6
ns-cn <ns-cn@qq.com>
2022-10-18 12:05:07 +0800

    1.0.7

--- Formula/consul-bridge.rb
+++ Formula/consul-bridge.rb
@@ -3,7 +3,7 @@ class ConsulBridge < Formula
   homepage "https://github.com/ns-cn/consul-bridge/"
   url "https://github.com/ns-cn/consul-bridge/releases/download/1.0.7/consul_bridge_darwin"
   version "1.0.7"
-  sha256 ""
+  sha256 "1aeb829a31e48be0ab398648ced56dd7351870e45296faf1dbf42c2935f78fad"
 
   def install
     system "mv consul_bridge_darwin consul-bridge"
