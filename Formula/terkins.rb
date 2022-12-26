178740be361c34cd6ec225a25a86d137ebd1f8d8
ns-cn <ns-cn@qq.com>
2022-12-26 16:56:26 +0800

    terkins 1.1.0

---
+++ Formula/terkins.rb
@@ -0,0 +1,16 @@
+class Terkins < Formula
+  desc "jenkins tools for job view or build"
+  homepage "https://github.com/ns-cn/ttools/"
+  url "https://github.com/ns-cn/ttools/releases/download/terkins-1.1.0/terkins_darwin"
+  version "1.1.0"
+  sha256 ""
+
+  def install
+    system "mv terkins_darwin terkins"
+    bin.install "terkins"
+  end
+
+  test do
+    system "false"
+  end
+end
