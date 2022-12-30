eb4416b1e7946bfc5a7d7d57cf58d42fb391ee6b
ns-cn <ns-cn@qq.com>
2022-12-30 15:40:13 +0800

    svnall 1.01->1.02

--- Formula/svnall.rb
+++ Formula/svnall.rb
@@ -1,8 +1,8 @@
 class Svnall < Formula
   desc "svn tools for multi repository"
   homepage "https://github.com/ns-cn/ttools/"
-  url "https://github.com/ns-cn/ttools/releases/download/v.svnall-1.01/svnall_darwin"
-  version "1.01"
+  url "https://github.com/ns-cn/svnall/releases/download/1.02/svnall_darwin"
+  version "1.02"
   sha256 ""
 
   def install
