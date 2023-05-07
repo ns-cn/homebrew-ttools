56b7dfa3c835f7ed422a3f66da6eb9cb229136b5
ns-cn <ns-cn@qq.com>
2023-05-07 20:44:01 +0800

    keeper

---
+++ Formula/keeper.rb
@@ -0,0 +1,16 @@
+class Keeper < Formula
+  desc "一个替换Linux中cron的程序"
+  homepage "https://github.com/ns-cn/keeper/"
+  url "https://kgithub.com/ns-cn/keeper/releases/download/1.2023.0427/keeper_darwin"
+  version "1.2023.0427"
+  sha256 ""
+
+  def install
+    system "mv keeper_darwin keeper"
+    bin.install "keeper"
+  end
+
+  test do
+    system "false"
+  end
+end
